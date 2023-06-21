8e1159ccba2813969151b33ab4b3db377790c3dc
ssandlin12 <ssandlin12@gmail.com>
2023-06-21 13:17:22 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+const express   = require("express");
+const hbs       = require("hbs");
+const app       = express();
+const path      = require("path");
+
+hbs.registerPartials("partials_absolute_path")
+hbs.registerPartials(__dirname + "/views/partials");
+
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "views"));
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res, next) => res.render("index"));
+
+app.get("/players", (req, res, next) => res.render("players"));
+
+app.get("/teams", (req, res, next) => res.render("teams"));
+
+app.listen(3000);
